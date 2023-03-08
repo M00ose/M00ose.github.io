de5d5b9b118e6c23de3af5242738539bc20ad8e0
Admin <tyrone.jacalan@protonmail.com>
2023-03-08 21:59:46 +0800

    Added colors, box shadow, and screen configs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{js,jsx}"],
+  mode: "jit",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#EDEADE",
+        secondary: "#878749",
+        tertiary: "#BABA9C",
+        "off-black": "#2e2e19",
+        "off-grey": "#e8e8d4",
+      },
+      boxShadow: {
+        card: "0px 35px 120px -15px #211e35",
+      },
+      screens: {
+        xs: "450px",
+      },
+    },
   },
   plugins: [],
-}
+};
