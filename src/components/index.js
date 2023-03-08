037648dc80d4d58b0e20c6155b38765ce3df9868
Admin <tyrone.jacalan@protonmail.com>
2023-03-08 21:53:04 +0800

    Initial commit

---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import Stack from "./Stack";
+import Projects from "./Projects";
+import Contact from "./Contact";
+
+export {
+    Navbar,
+    Hero,
+    Stack,
+    Projects,
+    Contact,
+};
---
+++ src/constants/index.js
@@ -0,0 +1,83 @@
+export const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "projects",
+      title: "projects",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+];
+
+const technologies = [
+    {
+      name: "HTML 5",
+      icon: html,
+    },
+    {
+      name: "CSS 3",
+      icon: css,
+    },
+    {
+      name: "JavaScript",
+      icon: javascript,
+    },
+    {
+      name: "TypeScript",
+      icon: typescript,
+    },
+    {
+      name: "React JS",
+      icon: reactjs,
+    },
+    {
+      name: "Tailwind CSS",
+      icon: tailwind,
+    },
+    {
+      name: "Node JS",
+      icon: nodejs,
+    },
+    {
+      name: "MongoDB",
+      icon: mongodb,
+    },
+    {
+      name: "Three JS",
+      icon: threejs,
+    },
+    {
+      name: "git",
+      icon: git,
+    },
+];
+
+const projects = [
+    {
+      name: "",
+      description:
+        "",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "mongodb",
+          color: "green-text-gradient",
+        },
+        {
+          name: "tailwind",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: "",
+      source_code_link: "",
+    },
+];
+
+export { technologies, projects };
