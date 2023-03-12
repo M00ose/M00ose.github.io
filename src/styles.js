df27cc0ec2c03f7a481dc1f60d9535e7ca82e591
Admin <tyrone.jacalan@protonmail.com>
2023-03-12 20:36:44 +0800

    Added infinity scroll classes

--- src/styles.js
+++ src/styles.js
@@ -1,24 +1,17 @@
 const styles = {
-    paddingX: "sm:px-12 px-20",
-    paddingY: "sm:py-12 py-6",
-    padding: "sm:px-12 px-20 sm:py-12 py-10",
+    paddingX: "px-6 md:px-12",
+    paddingY: "md:py-12 py-6",
+    padding: "px-6 md:px-12 md:py-12 py-10",
 
     flexRow: "flex flex-row gap-4",
     flexCol: "flex flex-col gap-4",
+    flexColRow: "flex flex-col md:flex-row gap-4",
+    flexRowCol: "flex flex-row md:flex-col gap-4",
 
     border: "border-off-black border-2",
 
-    tooltip: "absolute opacity-0 hover:opacity-1 bg-off-black inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 rounded-lg shadow-sm",
-  
-    // heroHeadText:
-    //   "font-black text-white lg:text-[80px] sm:text-[60px] xs:text-[50px] text-[40px] lg:leading-[98px] mt-2",
-    // heroSubText:
-    //   "text-[#dfd9ff] font-medium lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px]",
-  
-    // sectionHeadText:
-    //   "text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]",
-    // sectionSubText:
-    //   "sm:text-[18px] text-[14px] text-secondary uppercase tracking-wider",
+    infiniteScroll: 'animate-[scrollSM_20s_ease-in-out_infinite] md:animate-[scroll_20s_ease-in-out_infinite]',
+
   };
   
   export { styles };
