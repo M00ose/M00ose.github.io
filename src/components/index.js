584c7097ecc9b8447dd193198dd594f334aca337
Admin <tyrone.jacalan@protonmail.com>
2023-03-13 22:03:43 +0800

    Added MooseCanvas import

--- src/components/index.js
+++ src/components/index.js
@@ -2,10 +2,12 @@ import Navbar from "./Navbar";
 import Hero from "./Hero";
 import Projects from "./Projects";
 import Contact from "./Contact";
+import MooseCanvas from "./Moose";
 
 export {
     Navbar,
     Hero,
     Projects,
     Contact,
+    MooseCanvas,
 };
