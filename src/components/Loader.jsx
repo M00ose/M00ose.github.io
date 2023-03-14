cc5163dea6ed64669685da24c89ec95c2dfbde92
Admin <tyrone.jacalan@protonmail.com>
2023-03-14 18:36:12 +0800

    Initial commit

---
+++ src/components/Loader.jsx
@@ -0,0 +1,20 @@
+import { Html, useProgress } from '@react-three/drei';
+
+const Loader = () => {
+    const { progress } = useProgress();
+  return (
+    <Html>
+        <span className='canvas-loader'></span>
+        <p
+            style={{
+                fontSize: 14,
+                color: '#2e2e19',
+                fontWeight: 800,
+                marginTop: 40
+            }}
+        >{progress.toFixed(2)}%</p>
+    </Html>
+  )
+}
+
+export default Loader
