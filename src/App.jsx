e4ca9c9df70c008df5b084c02cfd6666db653f9b
Admin <tyrone.jacalan@protonmail.com>
2023-03-14 18:36:44 +0800

    Added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,17 @@
-import { BrowserRouter } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 import { Navbar, Hero, Projects, Contact } from "./components";
-import { projects } from "./constants";
 
 const App = () => {
   return (
-    <BrowserRouter>
       <div className="relative snap-y snap-mandatory h-screen w-screen overflow-scroll z-0 bg-primary text-off-black">
-        <div className='snap-start h-screen w-screen'>
-          <Navbar />
-          <Hero />
-        </div>
-        {projects.map((project) => (
-          <Projects 
-            name={project.name} 
-            description={project.description}
-            tag={project.tag}
-            tech={project.tech}
-            image={project.image}
-            link={project.link}
-            github={project.github}
-          />
-        ))}
-        
-        <Contact />
+        <Navbar />
+        <Routes>
+          <Route exact path="/about" element={ <Hero /> } />
+          <Route exact path="/projects" element={ <Projects /> } />
+          <Route exact path="/contact" element={<Contact />} />
+        </Routes>
       </div>
-    </BrowserRouter>
   )
 }
 
