e00a659942e17b9d50c30175083939e097910eb4
M00ose <tyrone.jacalan@protonmail.com>
2023-05-25 14:15:36 +0800

    Added antonpastoriza website in projects

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,47 +1,47 @@
-import menu from './menu.svg';
-import close from './close.svg';
-import profile from './profile.jpeg';
+import menu from "./menu.svg";
+import close from "./close.svg";
+import profile from "./profile.jpeg";
 
-import css3 from './technologies/css3.svg';
-import express from './technologies/express.svg';
-import figma from './technologies/figma.svg';
-import git from './technologies/git.svg';
-import html5 from './technologies/html5.svg';
-import javascript from './technologies/javascript.svg';
-import materialui from './technologies/materialui.svg';
-import mongoDB from './technologies/mongoDB.svg';
-import nextjs from './technologies/nextjs.svg';
-import node from './technologies/node.svg';
-import react from './technologies/react.svg';
-import tailwindcss from './technologies/tailwindcss.svg';
-import threejs from './technologies/threejs.svg';
-import typescript from './technologies/typescript.svg';
+import css3 from "./technologies/css3.svg";
+import express from "./technologies/express.svg";
+import figma from "./technologies/figma.svg";
+import git from "./technologies/git.svg";
+import html5 from "./technologies/html5.svg";
+import javascript from "./technologies/javascript.svg";
+import materialui from "./technologies/materialui.svg";
+import mongoDB from "./technologies/mongoDB.svg";
+import nextjs from "./technologies/nextjs.svg";
+import node from "./technologies/node.svg";
+import react from "./technologies/react.svg";
+import tailwindcss from "./technologies/tailwindcss.svg";
+import threejs from "./technologies/threejs.svg";
+import typescript from "./technologies/typescript.svg";
 
-import arienza from './projects/arienza.png';
-import prospectify from './projects/prospectify.png';
-import siargao from './projects/siargao.png';
+import arienza from "./projects/arienza.png";
+import prospectify from "./projects/prospectify.png";
+import siargao from "./projects/siargao.png";
+import pastoriza from "./projects/pastoriza.png";
 
 export {
-    menu,
-    close,
-    profile,
-
-    css3,
-    express,
-    figma,
-    git,
-    html5,
-    javascript,
-    materialui,
-    mongoDB,
-    nextjs,
-    node,
-    react,
-    tailwindcss,
-    threejs,
-    typescript,
-
-    arienza,
-    prospectify,
-    siargao,
-}
+  menu,
+  close,
+  profile,
+  css3,
+  express,
+  figma,
+  git,
+  html5,
+  javascript,
+  materialui,
+  mongoDB,
+  nextjs,
+  node,
+  react,
+  tailwindcss,
+  threejs,
+  typescript,
+  arienza,
+  prospectify,
+  siargao,
+  pastoriza,
+};
--- src/constants/index.js
+++ src/constants/index.js
@@ -13,116 +13,129 @@ import {
   tailwindcss,
   threejs,
   typescript,
-  
   arienza,
   prospectify,
   siargao,
-} from '../assets';
+  pastoriza,
+} from "../assets";
 
 export const navLinks = [
-    {
-      id: "",
-      title: "About",
-    },
-    {
-      id: "projects",
-      title: "Projects",
-    },
-    {
-      id: "contact",
-      title: "Contact",
-    },
+  {
+    id: "",
+    title: "About",
+  },
+  {
+    id: "projects",
+    title: "Projects",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
 ];
 
 const technologies = [
-    {
-      name: "HTML 5",
-      icon: html5,
-      description: "HTML is the standard markup language used to create web pages; it ensures proper formatting of text and images (using tags) so that Internet browsers can display them in the ways they were intended to look."
-    },
-    {
-      name: "CSS 3",
-      icon: css3,
-    },
-    {
-      name: "JavaScript",
-      icon: javascript,
-    },
-    {
-      name: "TypeScript",
-      icon: typescript,
-    },
-    {
-      name: "React.JS",
-      icon: react,
-    },
-    {
-      name: "Next.JS",
-      icon: nextjs,
-    },
-    {
-      name: "Tailwind CSS",
-      icon: tailwindcss,
-    },
-    {
-      name: "Material UI",
-      icon: materialui,
-    },
-    {
-      name: "Node.JS",
-      icon: node,
-    },
-    {
-      name: "Express",
-      icon: express,
-    },
-    {
-      name: "MongoDB",
-      icon: mongoDB,
-    },
-    {
-      name: "Three.JS",
-      icon: threejs,
-    },
-    {
-      name: "git",
-      icon: git,
-
-    },
-    {
-      name: "figma",
-      icon: figma,
-    },
+  {
+    name: "HTML 5",
+    icon: html5,
+    description:
+      "HTML is the standard markup language used to create web pages; it ensures proper formatting of text and images (using tags) so that Internet browsers can display them in the ways they were intended to look.",
+  },
+  {
+    name: "CSS 3",
+    icon: css3,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React.JS",
+    icon: react,
+  },
+  {
+    name: "Next.JS",
+    icon: nextjs,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwindcss,
+  },
+  {
+    name: "Material UI",
+    icon: materialui,
+  },
+  {
+    name: "Node.JS",
+    icon: node,
+  },
+  {
+    name: "Express",
+    icon: express,
+  },
+  {
+    name: "MongoDB",
+    icon: mongoDB,
+  },
+  {
+    name: "Three.JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
 ];
 
 const projects = [
-    {
-      name: "Siargao International Surfing Festival",
-      description: "A landing page showcasing the a surfing event in the Philippines. The goal was to design it to be fun, bright, and animated.",
-      tag: "Completed",
-      tech: 'Built with Next.js, Tailwind CSS, and Framer Motion.',
-      image: siargao,
-      link: "https://siargaoisf.vercel.app/",
-      github: "https://github.com/M00ose/webpage-siargaoISF",
-    },
-    {
-      name: "Arienza - Architects and Engineers",
-      description: "A modern and sleek landing page for an architecture and engineering firm.",
-      tag: "In progress",
-      tech: 'Built with React.js, Tailwind CSS, React-Router, Three.js, and Framer Motion.',
-      image: arienza,
-      link: "",
-      github: "https://github.com/M00ose/webpage-arienza",
-    },
-    {
-      name: "Prospectify",
-      description: "A CRUD application built with the MERN stack. The application is a CRM platform designed specifically for nonprofits and fundraising workflows.",
-      tag: "In progress",
-      tech: 'Built with the M.E.R.N stack, with Flowbite for design and Refine for development.',
-      image: prospectify,
-      link: "",
-      github: "https://github.com/M00ose/webapp-echo",
-    },
+  {
+    name: "Siargao International Surfing Festival",
+    description:
+      "A landing page showcasing the a surfing event in the Philippines. The goal was to design it to be fun, bright, and animated.",
+    tag: "Completed",
+    tech: "Built with Next.js, Tailwind CSS, and Framer Motion.",
+    image: siargao,
+    link: "https://siargaoisf.vercel.app/",
+    github: "https://github.com/M00ose/webpage-siargaoISF",
+  },
+  {
+    name: "Arienza - Architects and Engineers",
+    description:
+      "A modern and sleek landing page for an architecture and engineering firm.",
+    tag: "In progress",
+    tech: "Built with React.js, Tailwind CSS, React-Router, Three.js, and Framer Motion.",
+    image: arienza,
+    link: "",
+    github: "https://github.com/M00ose/webpage-arienza",
+  },
+  {
+    name: "Anton Pastoriza",
+    description:
+      "A simple artist gallery with an opening animation and interactive features.",
+    tag: "Completed",
+    tech: "Built with NExt.js, Tailwind CSS, and Framer Motion.",
+    image: pastoriza,
+    link: "",
+    github: "https://github.com/M00ose/webpage-antonPastoriza",
+  },
+  {
+    name: "Prospectify",
+    description:
+      "A CRUD application built with the MERN stack. The application is a CRM platform designed specifically for nonprofits and fundraising workflows.",
+    tag: "In progress",
+    tech: "Built with the M.E.R.N stack, with Flowbite for design and Refine for development.",
+    image: prospectify,
+    link: "https://webpage-anton-pastoriza.vercel.app/",
+    github: "https://github.com/M00ose/webapp-echo",
+  },
 ];
 
 export { technologies, projects };
